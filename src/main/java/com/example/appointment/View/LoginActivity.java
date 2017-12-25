6b5d7acb003bc75c3860e12dbf78a4114942498b
LJT123123 <1770841556@qq.com>
2017-12-25 16:28:04 +0800

    ljt test

--- src/main/java/com/example/appointment/View/LoginActivity.java
+++ src/main/java/com/example/appointment/View/LoginActivity.java
@@ -147,6 +147,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                         }
                     }
                 });
+
                 if(pref.getBoolean("valid",false)){
                     if (rememberPass.isChecked()){
                         editor.putBoolean("remember_password",true);
